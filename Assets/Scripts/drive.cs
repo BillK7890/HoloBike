610f54caa08d1290005dd9b430059ba1127ca5d9
Bill <wjk7890@rit.edu>
2017-06-26 16:31:47 -0400

    added plane and wall and movement.

--- Assets/Scripts/drive.cs
+++ Assets/Scripts/drive.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class drive : MonoBehaviour {
+    public float movetime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private Vector3 bikeMovement;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	protected virtual void Start () {
+        bikeMovement = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        bikeMovement.x -= 0.1f;
+        transform.position = new Vector3(bikeMovement.x, transform.position.y, transform.position.z);
 	}
 }
