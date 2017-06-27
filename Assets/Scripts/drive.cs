89666e28ab6e6d5a742156c58523113997e65236
Erik Azzarano <easports722@gmail.com>
2017-06-27 09:46:24 -0400

    Started working on player rotation

--- Assets/Scripts/drive.cs
+++ Assets/Scripts/drive.cs
@@ -7,7 +7,8 @@ public class drive : MonoBehaviour {
     public LayerMask blockingLayer;
 
     private Vector3 bikeMovement;
-
+	private float bikeRotation;
+	private float totalRotation;
 
 	// Use this for initialization
 	protected virtual void Start () {
@@ -16,7 +17,23 @@ public class drive : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.F)){
+			totalRotation += 45;
+		}
+		else if(Input.GetKeyDown(KeyCode.G)){
+			totalRotation += (-45);
+		}
+
+		//float bikeVertical = Input.GetAxis ("Vertical") * 90;
+
+		//float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+
+		//Quaternion target = Quaternion.Euler(0, bikeRotation, 0);
+		transform.rotation = new Quaternion(0, totalRotation, 0, 1);
+
+
         bikeMovement.x -= 0.1f;
-        transform.position = new Vector3(bikeMovement.x, transform.position.y, transform.position.z);
+        //transform.position = new Vector3(bikeMovement.x, transform.position.y, transform.position.z);
 	}
 }
