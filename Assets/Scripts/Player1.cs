2226bcd98294d52a423ae0e7aa9e9020258f24a3
DESKTOP-COP2RHQ\Microsoft Reactor <quick step>
2017-06-27 09:44:08 -0400

    no message

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.rotation = QualityLev
+        }
+		transform.Translate(Input.GetAxis)
+	}
+}
